2dd2dde324ed3b113b594c3c465352486d4ef9b1
Hamelinny <anastasiyasofr@yandex.ru>
2017-11-25 11:35:35 +0300

    123abc now processing correctly

--- src/main/java/lexer/LexStream.java
+++ src/main/java/lexer/LexStream.java
@@ -11,7 +11,6 @@ import java.util.Iterator;
 public class LexStream implements Iterator<Lexeme>{
     private Lexer lexer;
     private Lexeme next;
-    private Lexeme prev = null;
 
     public LexStream(java.io.Reader in) {
         lexer = new Lexer(in);
@@ -45,17 +44,6 @@ public class LexStream implements Iterator<Lexeme>{
 
         Lexeme lexeme = next;
         next = null;
-        if (prev != null) {
-            if (prev.getClass() == LiteralFloat.class && prev.getLine() == lexeme.getLine()
-                && prev.getEnd() == lexeme.getStart() && lexeme.getClass() != Delimiter.class) {
-                try {
-                    throw new NoSuchLexemeException(prev.getLine(), prev.getStart());
-                } catch (NoSuchLexemeException e) {
-                    e.printInfo();
-                }
-            }
-        }
-        prev = lexeme;
         return lexeme;
     }
 }
